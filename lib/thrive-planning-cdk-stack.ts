cec684ba51e1dbac327d87f157761142c9398939
Robert Smith <rob.smithw@hotmail.com>
2022-04-10 23:16:10 -0500

    Initial commit

--- lib/thrive-planning-cdk-stack.ts
+++ lib/thrive-planning-cdk-stack.ts
@@ -1,16 +1,40 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import { aws_cognito as cognito } from 'aws-cdk-lib';
+import { aws_ses as ses } from 'aws-cdk-lib';
+import { VerifySesEmailAddress } from '@seeebiii/ses-verify-identities'
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+let thrivPlanUserPoolName: string = 'ThrivPlanUsers'
+let thrivPlanEmail: string = 'thrivplan@gmail.com'
 
 export class ThrivePlanningCdkStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    new VerifySesEmailAddress(this, 'SesEmailVerification', {
+      emailAddress: thrivPlanEmail
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'ThrivePlanningCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new cognito.UserPool(this, thrivPlanUserPoolName, 
+      {
+        userPoolName: thrivPlanUserPoolName,
+        selfSignUpEnabled: false,
+        signInAliases: {
+          username: true
+        },
+        signInCaseSensitive: false,
+        standardAttributes: {
+          email: {
+            required: true
+          }
+        },
+        email: cognito.UserPoolEmail.withSES({
+          sesRegion: 'us-west-1',
+          fromEmail: thrivPlanEmail,
+          fromName: 'Thriv Plan'
+        })
+      }
+    );
   }
 }
